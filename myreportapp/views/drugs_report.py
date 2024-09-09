1ad54f85ab59c7a8d06402640a0465c94a56116c
Marcos-Capristo <marcos.capristo@gmail.com>
2024-09-09 00:50:22 -0300

    ajustes no drugs_report.html

--- myreportapp/views/drugs_report.py
+++ myreportapp/views/drugs_report.py
@@ -14,19 +14,38 @@ def drugs_report(request):
     rounded_time = now.replace(minute=minutes, second=0, microsecond=0)
     formatted_current_time = rounded_time.strftime('%H:%M')
     
-    # Subtrair uma hora
+    # Subtrair uma hora e meia
     before_time = rounded_time - timedelta(hours=1.5)
     formatted_before_time = before_time.strftime('%H:%M')
-    msg_about_this_form_to_user = f'{user.full_name},<br>Este formulário serve como uma alternativa caso o Sistema GDL não esteja disponível. Preencha todos os campos com atenção, verifique todos os dados cuidadosamente, salve o laudo em formato PDF e assine-o com seu certificado digital. Caso o laudo seja enviado à autoridade requisitante, ele deverá ser registrado oportunamente no Sistema GDL.'
     
+    # Obter a data de hoje
     today_date = now.date()
     
+    # Verificar se o horário anterior está em um dia anterior
+    if before_time.day != now.day:
+        before_date = today_date - timedelta(days=1)  # Se `before_time` for de um dia anterior
+    else:
+        before_date = today_date  # Se for do mesmo dia
+    
+    # Mensagem para o usuário
+    msg_about_this_form_to_user = (
+        f'{user.full_name},<br>Este formulário serve como uma alternativa '
+        'caso o Sistema GDL não esteja disponível. Preencha todos os campos com atenção, '
+        'verifique todos os dados cuidadosamente, salve o laudo em formato PDF e '
+        'assine-o com seu certificado digital. Caso o laudo seja enviado à autoridade requisitante, '
+        'ele deverá ser registrado oportunamente no Sistema GDL.'
+    )
+    
+    # Contexto para renderizar o template
     context = {
+        'protocol_prefix': 'TOX',
         'delegacia': 'Del. Sec. Limeira Plantão',
         'today_date': today_date,
         'current_time': formatted_current_time,
         'before_time': formatted_before_time,
+        'before_date': before_date,  # Adicionando before_date ao contexto
         'user_name': user.full_name,
         'msg_about_this_form_to_user': msg_about_this_form_to_user,
     }
+    
     return render(request, 'drugs_report.html', context)
