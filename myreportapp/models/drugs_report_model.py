49afc8bb2fadb73d671b441e381880d43911e8df
Marcos-Capristo <marcos.capristo@gmail.com>
2024-09-19 19:34:46 -0300

    drugs_report_model.py, campos para imagens, legendas e observações sobre material devolvido.

--- myreportapp/models/drugs_report_model.py
+++ myreportapp/models/drugs_report_model.py
@@ -2,10 +2,14 @@ from django.db import models
 from .base_report_model import BaseReport
 
 class DrugReport(BaseReport):
-    allSubstances = models.TextField(blank=True, default='')
-    materialReceivedObservations = models.TextField(blank=True, verbose_name='Todas as substâncias', default='')
+    allSubstances = models.TextField(blank=True, verbose_name='Substâncias examinadas', default='')
+    materialReceivedObservations = models.TextField(blank=True, verbose_name='Observações sobre o material recebido', default='')
+
+    # Imagem do material e sua respectiva legenda
+    materialImage = models.ImageField(upload_to='material_images/', blank=True, verbose_name='Fotografia do material')
+    materialImageCaption = models.TextField(blank=True, verbose_name='Legenda da fotografia do material', default='')
+
     listOfEnvolvedPeople = models.TextField(blank=True, verbose_name='Pessoas envolvidas', default='')
-    # listItensLabels = models.TextField(blank=True, verbose_name='Itens', default='') não tem função
     listOfPackagings = models.TextField(blank=True, verbose_name='Embalagens', default='')
     listOfMorphology = models.TextField(blank=True, verbose_name='Morfologia', default='')
     listOfGrossMass = models.TextField(blank=True, verbose_name='Massa bruta', default='') 
@@ -16,6 +20,19 @@ class DrugReport(BaseReport):
     listOfExitseal = models.TextField(blank=True, verbose_name='Lacre de saída', default='')
     listOfpackagingAndMorphology = models.TextField(blank=True, verbose_name='Descrição do item', default='')
     listOfResultOfExams = models.TextField(blank=True, verbose_name='Resultado do exame', default='')
+    listOfReturneds = models.TextField(blank=True, verbose_name='Material retornado e contraperícia', default='')
+
+    # Lista de imagens e legendas
+    examImages = models.ImageField(upload_to='exam_images/', blank=True, verbose_name='Imagens dos exames')
+    examImageCaptions = models.TextField(blank=True, verbose_name='Legendas das imagens dos exames', default='')
+
+    # Imagem dos itens retornados e legenda
+    returnedItemsImage = models.ImageField(upload_to='returned_items_images/', blank=True, verbose_name='Imagem dos itens retornados')
+    returnedItemsCaption = models.TextField(blank=True, verbose_name='Legenda dos itens retornados', default='')
+
+    # Imagem da contraperícia e legenda
+    counterProofImage = models.ImageField(upload_to='counterproof_images/', blank=True, verbose_name='Imagem da contraperícia')
+    counterProofCaption = models.TextField(blank=True, verbose_name='Legenda da contraperícia', default='')
 
     def save(self, *args, **kwargs):
         # Nenhuma conversão necessária, apenas chamar o método pai
