79e4e5c197b30b9cb5bf026c30777908022c7545
Marcos-Capristo <marcos.capristo@gmail.com>
2024-09-29 14:52:32 -0300

    formulário para configurações do usuário

--- myreportapp/forms/user_edit_profile_form.py
+++ myreportapp/forms/user_edit_profile_form.py
@@ -1,11 +1,43 @@
+"""
+
+Copyright (c) 2024 Marcos de Oliveira Capristo
+Todos os direitos reservados.
+
+MYREPORT é um projeto independente.
+Oferece um ambiente para edição de laudos periciais,
+voltado especialmente para Peritos Criminais Oficiais do Estado de São Paulo.
+Idealizado e inicialmente desenvolvido pelo Perito Criminal Marcos de Oliveira Capristo.
+Contato: marcos.moc@policiacientifica.sp.gov.br | (19) 9 8231-2774
+
+
+"""
+
+"""
+
+FORM PARA CONFIGURAÇÕES DO USUÁRIO
+"""
+
+
+
 from django import forms
-#from .models import UserRegistrationModel
+from django.contrib.auth.forms import PasswordChangeForm  # Importa o formulário de alteração de senha
 from ..models import UserRegistrationModel
 
 class EditProfileForm(forms.ModelForm):
+    new_password = forms.CharField(
+        label='Nova Senha',
+        widget=forms.PasswordInput,
+        required=False,  # Senha não é obrigatória, a menos que o usuário queira alterá-la
+    )
+    confirm_password = forms.CharField(
+        label='Confirmar Senha',
+        widget=forms.PasswordInput,
+        required=False,  # Senha não é obrigatória
+    )
+
     class Meta:
         model = UserRegistrationModel
-        fields = ['full_name', 'city', 'unit', 'team', 'image']  # Inclua os campos que podem ser alterados
+        fields = ['full_name', 'city', 'unit', 'team', 'image', 'new_password', 'confirm_password']  # Inclua os novos campos
         labels = {
             'full_name': 'Nome Completo',
             'city': 'Cidade',
@@ -16,4 +48,6 @@ class EditProfileForm(forms.ModelForm):
         help_texts = {
             'full_name': 'Inclua o nome como deseja que apareça no laudo, incluindo Dr. ou Dra.',
             'image': 'Envie uma imagem para o perfil. Formato suportado: JPEG, PNG.',
+            'new_password': 'Deixe em branco se não quiser alterar a senha.',
+            'confirm_password': 'Deixe em branco se não quiser alterar a senha.',
         }
--- myreportapp/views/edit_profile.py
+++ myreportapp/views/edit_profile.py
@@ -14,24 +14,39 @@ Contato: marcos.moc@policiacientifica.sp.gov.br | (19) 9 8231-2774
 
 """
 
-VIEW CONCTACT, A DESENVOLVER
-
+VIEW PARA CONFIGURAÇÕES DO USUÁRIO
 
 """
+
 
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash  # Importa a função para atualizar a sessão
 from ..forms import EditProfileForm
 
 @login_required
 def editProfile(request):
     if request.method == 'POST':
         form = EditProfileForm(request.POST, request.FILES, instance=request.user)
+        
+        # Obtenha a nova senha e a confirmação da senha do formulário
+        new_password = request.POST.get('new_password')
+        confirm_password = request.POST.get('confirm_password')
+
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Seu perfil foi atualizado com sucesso!')
+            form.save()  # Salva as alterações no perfil
+
+            # Verifica se o usuário deseja alterar a senha
+            if new_password and new_password == confirm_password:
+                request.user.set_password(new_password)  # Altera a senha do usuário
+                request.user.save()  # Salva o usuário com a nova senha
+                update_session_auth_hash(request, request.user)  # Mantém o usuário logado após a alteração da senha
+                messages.success(request, 'Seu perfil foi atualizado com sucesso e a senha foi alterada!')
+            else:
+                messages.error(request, 'As senhas não correspondem ou estão vazias.')
+
             return redirect('edit_profile')  # Redireciona para a página de edição após salvar
         else:
             messages.error(request, 'Por favor, corrija os erros abaixo.')
