a9e8d051dc5cc50cb5ff2613d91a56d9e55ae1f6
Marcos-Capristo <marcos.capristo@gmail.com>
2024-10-23 15:27:05 -0300

    ajustes em propertyreport, html, model e view

--- myreportapp/models/property_report_model.py
+++ myreportapp/models/property_report_model.py
@@ -2,7 +2,11 @@ from django.db import models
 from ..models import HeaderReportModel
 
 class PropertyReportModel(models.Model):
+    title = models.CharField(max_length=200, blank=True, default='')
+    subtitle = models.CharField(max_length=200, blank=True, default='')
     description = models.TextField(blank=True, verbose_name='Imóvel - Descrição', default='')
+    image = models.ImageField(blank=True, null=True, upload_to='property_images/', default='')
+    legend = models.CharField(max_length=200, blank=True, default='')
     reportForeignKey = models.ForeignKey(
         HeaderReportModel, 
         on_delete=models.CASCADE,  
--- myreportapp/views/property_report_view.py
+++ myreportapp/views/property_report_view.py
@@ -4,43 +4,42 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def PropertyReportView(request, report_id):
-    # Obtém o cabeçalho do relatório associado ao usuário logado
     report = get_object_or_404(HeaderReportModel, id=report_id, reporting_expert=request.user)
 
-    # Inicialmente tenta buscar o relatório de preservação, se ele existir
     try:
-        # Verifica se já existe um relatório de imóvel vinculado ao cabeçalho
         property = PropertyReportModel.objects.get(reportForeignKey=report)
-        print(f'Valor da chave estrangeira = {property.reportForeignKey}, id = {report.id}')
     except PropertyReportModel.DoesNotExist:
-        # Se não existir, inicializa como None para criar depois
         property = None
-        print(f'não há ainda descrição de imóvel para o laudo {report.id}')
 
-    # Se o método for POST, significa que o formulário foi enviado
     if request.method == 'POST':
+        property_title = request.POST.get('title')
+        property_subtitle = request.POST.get('subtitle')
         property_description = request.POST.get('property')
+        property_image = request.FILES.get('image')
+        property_legend = request.POST.get('legend')
 
-        # Divide o texto em parágrafos (usando \n como delimitador) e remove parágrafos vazios
         paragraphs = [p.strip() for p in property_description.split('\n') if p.strip()]
 
         if property:
-            # Se o relatório já existir, atualiza os dados existentes
-            property.description = '\n'.join(paragraphs)  # Salva os parágrafos unidos
+            property.title = property_title  # Atualiza o título
+            property.subtitle = property_subtitle  # Atualiza o subtítulo
+            property.description = '\n'.join(paragraphs)  # Atualiza a descrição
+            property.legend = property_legend
+            if property_image:  # Atualiza a imagem se uma nova foi enviada
+                property.image = property_image
         else:
-            # Se não existir, cria um novo registro
             property = PropertyReportModel(
                 reportForeignKey=report,
-                description='\n'.join(paragraphs)  # Salva os parágrafos unidos
+                title=property_title,  # Salva o título
+                subtitle=property_subtitle,  # Salva o subtítulo
+                description='\n'.join(paragraphs),  # Salva a descrição
+                image=property_image, # Salva a imagem se fornecida
+                legend=property_legend,
             )
         
-        # Salva o relatório (seja criação ou atualização)
         property.save()
+        return redirect('modules_report', report.id)
 
-        # Redireciona para a página desejada após o salvamento
-        return redirect('modules_report', report.id)  # Ajuste o nome da view de destino
-
-    # Se for uma requisição GET (ou outra), renderiza o formulário
     context = {
         'report': report,
         'property': property,
